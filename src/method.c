4363044c0acdd54ff6c26e89c70c6741b6d09e25
Max Kellermann <mk@cm4all.com>
2012-08-02 16:34:49 +0200

    method: add WebDAV methods

--- src/method.c
+++ src/method.c
@@ -12,4 +12,13 @@ const char *http_method_to_string_data[HTTP_METHOD_INVALID] = {
     [HTTP_METHOD_DELETE] = "DELETE",
     [HTTP_METHOD_OPTIONS] = "OPTIONS",
     [HTTP_METHOD_TRACE] = "TRACE",
+
+    /* WebDAV methods */
+    [HTTP_METHOD_PROPFIND] = "PROPFIND",
+    [HTTP_METHOD_PROPPATCH] = "PROPPATCH",
+    [HTTP_METHOD_MKCOL] = "MKCOL",
+    [HTTP_METHOD_COPY] = "COPY",
+    [HTTP_METHOD_MOVE] = "MOVE",
+    [HTTP_METHOD_LOCK] = "LOCK",
+    [HTTP_METHOD_UNLOCK] = "UNLOCK",
 };
