7aa52b7dc89c23634483ad371c5bb2e0241ff191
Max Kellermann <max@duempel.org>
2015-02-03 15:30:25 +0100

    header: optimize http_header_is_hop_by_hop()

--- src/header.c
+++ src/header.c
@@ -29,15 +29,32 @@ http_header_is_hop_by_hop(const char *name)
 {
     assert(name != NULL);
 
-    return strcmp(name, "connection") == 0 ||
-        strcmp(name, "keep-alive") == 0 ||
-        strcmp(name, "proxy-authenticate") == 0 ||
-        strcmp(name, "proxy-authorization") == 0 ||
-        strcmp(name, "te") == 0 ||
-        /* typo in RFC 2616? */
-        strcmp(name, "trailer") == 0 || strcmp(name, "trailers") == 0 ||
-        strcmp(name, "upgrade") == 0 ||
-        strcmp(name, "expect") == 0 || /* RFC 2616 14.20 */
-        strcmp(name, "transfer-encoding") == 0 ||
-        strcmp(name, "content-length") == 0;
+    switch (*name) {
+    case 'c':
+        return strcmp(name, "connection") == 0 ||
+            strcmp(name, "content-length") == 0;
+
+    case 'e':
+        /* RFC 2616 14.20 */
+        return strcmp(name, "expect") == 0;
+
+    case 'k':
+        return strcmp(name, "keep-alive") == 0;
+
+    case 'p':
+        return strcmp(name, "proxy-authenticate") == 0 ||
+            strcmp(name, "proxy-authorization") == 0;
+
+    case 't':
+        return strcmp(name, "te") == 0 ||
+            /* typo in RFC 2616? */
+            strcmp(name, "trailer") == 0 || strcmp(name, "trailers") == 0 ||
+            strcmp(name, "transfer-encoding") == 0;
+
+    case 'u':
+        return strcmp(name, "upgrade") == 0;
+
+    default:
+        return false;
+    }
 }
