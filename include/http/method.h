dbd85373f796d9a2e96bc463ee788def92ebbd37
Max Kellermann <mk@cm4all.com>
2024-11-26 18:03:46 +0100

    indent with tabs

--- include/http/method.h
+++ include/http/method.h
@@ -8,31 +8,31 @@
 #include <assert.h>
 
 typedef enum {
-    HTTP_METHOD_NULL = 0,
-    HTTP_METHOD_HEAD,
-    HTTP_METHOD_GET,
-    HTTP_METHOD_POST,
-    HTTP_METHOD_PUT,
-    HTTP_METHOD_DELETE,
-    HTTP_METHOD_OPTIONS,
-    HTTP_METHOD_TRACE,
+	HTTP_METHOD_NULL = 0,
+	HTTP_METHOD_HEAD,
+	HTTP_METHOD_GET,
+	HTTP_METHOD_POST,
+	HTTP_METHOD_PUT,
+	HTTP_METHOD_DELETE,
+	HTTP_METHOD_OPTIONS,
+	HTTP_METHOD_TRACE,
 
-    /* WebDAV methods */
-    HTTP_METHOD_PROPFIND,
-    HTTP_METHOD_PROPPATCH,
-    HTTP_METHOD_MKCOL,
-    HTTP_METHOD_COPY,
-    HTTP_METHOD_MOVE,
-    HTTP_METHOD_LOCK,
-    HTTP_METHOD_UNLOCK,
+	/* WebDAV methods */
+	HTTP_METHOD_PROPFIND,
+	HTTP_METHOD_PROPPATCH,
+	HTTP_METHOD_MKCOL,
+	HTTP_METHOD_COPY,
+	HTTP_METHOD_MOVE,
+	HTTP_METHOD_LOCK,
+	HTTP_METHOD_UNLOCK,
 
-    /* RFC 5789 */
-    HTTP_METHOD_PATCH,
+	/* RFC 5789 */
+	HTTP_METHOD_PATCH,
 
-    /* Versioning Extensions to WebDAV methods (RFC3253) */
-    HTTP_METHOD_REPORT,
+	/* Versioning Extensions to WebDAV methods (RFC3253) */
+	HTTP_METHOD_REPORT,
 
-    HTTP_METHOD_INVALID,
+	HTTP_METHOD_INVALID,
 } http_method_t;
 
 extern const char *http_method_to_string_data[HTTP_METHOD_INVALID];
@@ -40,23 +40,23 @@ extern const char *http_method_to_string_data[HTTP_METHOD_INVALID];
 static inline bool
 http_method_is_valid(http_method_t method)
 {
-    return method > HTTP_METHOD_NULL && method < HTTP_METHOD_INVALID;
+	return method > HTTP_METHOD_NULL && method < HTTP_METHOD_INVALID;
 }
 
 static inline bool
 http_method_is_empty(http_method_t method)
 {
-    /* RFC 2616 4.3: "All responses to the HEAD request method MUST
-       NOT include a message-body, even though the presence of entity
-       header fields might lead one to believe they do." */
-    return method == HTTP_METHOD_HEAD;
+	/* RFC 2616 4.3: "All responses to the HEAD request method MUST
+	   NOT include a message-body, even though the presence of entity
+	   header fields might lead one to believe they do." */
+	return method == HTTP_METHOD_HEAD;
 }
 
 static inline const char *
 http_method_to_string(http_method_t method)
 {
-    assert(method < sizeof(http_method_to_string_data) / sizeof(http_method_to_string_data[0]));
-    assert(http_method_to_string_data[method]);
+	assert(method < sizeof(http_method_to_string_data) / sizeof(http_method_to_string_data[0]));
+	assert(http_method_to_string_data[method]);
 
-    return http_method_to_string_data[method];
+	return http_method_to_string_data[method];
 }
--- src/header.c
+++ src/header.c
@@ -10,51 +10,51 @@
 static inline bool
 http_header_name_char_valid(char ch)
 {
-    return (signed char)ch > 0x20 && ch != ':';
+	return (signed char)ch > 0x20 && ch != ':';
 }
 
 bool
 http_header_name_valid(const char *name)
 {
-    do {
-        if (!http_header_name_char_valid(*name))
-            return false;
-    } while (*++name != 0);
+	do {
+		if (!http_header_name_char_valid(*name))
+			return false;
+	} while (*++name != 0);
 
-    return true;
+	return true;
 }
 
 bool
 http_header_is_hop_by_hop(const char *name)
 {
-    assert(name != NULL);
+	assert(name != NULL);
 
-    switch (*name) {
-    case 'c':
-        return strcmp(name, "connection") == 0 ||
-            strcmp(name, "content-length") == 0;
+	switch (*name) {
+	case 'c':
+		return strcmp(name, "connection") == 0 ||
+		       strcmp(name, "content-length") == 0;
 
-    case 'e':
-        /* RFC 2616 14.20 */
-        return strcmp(name, "expect") == 0;
+	case 'e':
+		/* RFC 2616 14.20 */
+		return strcmp(name, "expect") == 0;
 
-    case 'k':
-        return strcmp(name, "keep-alive") == 0;
+	case 'k':
+		return strcmp(name, "keep-alive") == 0;
 
-    case 'p':
-        return strcmp(name, "proxy-authenticate") == 0 ||
-            strcmp(name, "proxy-authorization") == 0;
+	case 'p':
+		return strcmp(name, "proxy-authenticate") == 0 ||
+		       strcmp(name, "proxy-authorization") == 0;
 
-    case 't':
-        return strcmp(name, "te") == 0 ||
-            /* typo in RFC 2616? */
-            strcmp(name, "trailer") == 0 || strcmp(name, "trailers") == 0 ||
-            strcmp(name, "transfer-encoding") == 0;
+	case 't':
+		return strcmp(name, "te") == 0 ||
+		       /* typo in RFC 2616? */
+		       strcmp(name, "trailer") == 0 || strcmp(name, "trailers") == 0 ||
+		       strcmp(name, "transfer-encoding") == 0;
 
-    case 'u':
-        return strcmp(name, "upgrade") == 0;
+	case 'u':
+		return strcmp(name, "upgrade") == 0;
 
-    default:
-        return false;
-    }
+	default:
+		return false;
+	}
 }
